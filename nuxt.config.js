4c584de99ab29e97e7913a43f2dd084e33c03e21
jackcunningham <jacktcunningham@gmail.com>
2021-10-23 14:41:35 +0100

    remove link

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,6 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    '@/assets/css/variables.scss'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
