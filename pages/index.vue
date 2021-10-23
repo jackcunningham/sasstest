2567e2cc0a88c46b768034f3a1a204ed5fc16e0c
jackcunningham <jacktcunningham@gmail.com>
2021-10-23 13:58:39 +0100

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <h1>SCSS Test</h1>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="css" scoped>
+.home {
+    color: yellow;
+}
+</style>
+
